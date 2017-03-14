8f642516d7a8835bef0cf87a41c44396ac58755b
MrCarlosGomez <solrac1carlos@gmail.com>
2017-03-14 16:55:07 +0100

    Have added a date selector
    Have added a date selector

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -14,6 +14,32 @@ angular.module('goalController', [])
       $scope.goals = updateGoals(goals, day);
       $scope.day = day.day;
     });
+    webshim.setOptions('forms-ext', {
+       replaceUI: 'auto',
+       types: 'date',
+       date: {
+           startView: 2,
+           inlinePicker: true,
+           classes: 'hide-inputbtns'
+       }
+   });
+   webshim.setOptions('forms', {
+       lazyCustomMessages: true
+   });
+   //start polyfilling
+   webshim.polyfill('forms forms-ext');
+
+   //only last example using format display
+   $(function () {
+       $('.format-date').each(function () {
+           var $display = $('.date-display', this);
+           $(this).on('change', function (e) {
+               //webshim.format will automatically format date to according to webshim.activeLang or the browsers locale
+               var localizedDate = webshim.format.date($.prop(e.target, 'value'));
+               $display.html(localizedDate);
+           });
+       });
+   });
   };
   updateGoals = function (goals, day) {
     for (var i=0; i<goals.length; i++) {
